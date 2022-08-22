a9fac6bc0d69ff66db4f038e18df86a850f8bcab
dinofree0618 <lovenovel618@gmail.com>
2022-08-23 00:19:44 +0800

    添加多國語

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// I18n
+import vueI18n from "./plugin/lang";
 
 const app = createApp(App);
 const store = createPinia();
 app.use(store);
 app.use(router);
-
+app.use(vueI18n);
 app.mount("#app");
---
+++ src/plugin/lang/index.ts
@@ -0,0 +1,19 @@
+import { createI18n } from "vue-i18n";
+import tw from "@/assets/lang/zh_TW";
+import cn from "@/assets/lang/zh_CN";
+import en from "@/assets/lang/en_US";
+
+export const i18n = createI18n({
+  legacy: false,
+  // 如果本地有语言标识就采用本地，没有就根据浏览器语言默认标识显示语言
+  // locale: localStorage.getItem("locale") || navigator.language.slice(0, 2),
+  locale: "zh_TW",
+  fallbackLocale: "en_US",
+  messages: {
+    "zh_TW": tw,
+    "en_US": en,
+    "zh_CN": cn
+  },
+});
+
+export default i18n;
